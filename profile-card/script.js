6a1d3267647cfdb3a6155457c63ce1f90e1db8e6
andreealica3 <andreealica67@gmail.com>
2025-05-12 22:23:16 +0300

    Clamp

---
+++ profile-card/script.js
@@ -0,0 +1,8 @@
+  document.querySelector('.follow-btn').addEventListener('click', function () {
+    this.classList.toggle('following');
+  });
+
+  document.querySelector('.follow-third').addEventListener('click', function () {
+    this.classList.toggle('unfollowing');
+  });
+
