988c442300afc870d75bd92284153822528fcb81
andreealica3 <andreealica67@gmail.com>
2025-06-19 18:12:00 +0300

    Start

--- First-Website/profile-card/script.js
+++
@@ -1,9 +0,0 @@
-  document.querySelector('.follow-btn').addEventListener('click', function () {
-    this.classList.toggle('following');
-  });
-
-  document.querySelector('.follow-third').addEventListener('click', function () {
-    this.classList.toggle('unfollowing');
-  });
-
-
